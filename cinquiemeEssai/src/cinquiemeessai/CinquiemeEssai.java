b6ee22138a59f1d9b0db5ab3dc98b3ea51458419
Roni Hanna <roni.hanna@isae.edu.lb>
2023-03-20 19:19:23 +0200

    Creation de cinquiemeEssai

---
+++ cinquiemeEssai/src/cinquiemeessai/CinquiemeEssai.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cinquiemeessai;
+
+public class CinquiemeEssai {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 7;
+
+        int z = x * y;
+        
+        System.out.println(z);
+        
+    }
+
+}
